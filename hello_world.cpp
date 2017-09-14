14a7345d3919313e6b1b05ca5de5433dade180e3
Alireza Kheirkhahan <kheirkhahan@gmail.com>
2017-09-14 16:31:49 -0500

    initial commit

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+//  Copyright (c) 2017 Alireza Kheirkhahan
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/iostreams.hpp>
+
+int main()
+{
+    // Say hello to the world!
+    hpx::cout << "Hello World!\n" << hpx::flush;
+    return 0;
+}
